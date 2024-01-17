ac50da17d30fbb845cebf17123024a16490522a0
Rahul Nai <36158319+senrahulroy@users.noreply.github.com>
2024-01-17 23:50:25 +0530

    - OnBoarding Commit and Push Again.

---
+++ lib/features/auth/screens/onboarding/onboarding.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class OnBoardingScreen extends StatelessWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('hy i m rahul'),
+      ),
+    );
+  }
+}
